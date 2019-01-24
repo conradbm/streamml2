4ed5a15e843ed05b2dcdcda8a7952c38f337dc2e
Blake Conrad <bmc.cs@outlook.com>
2019-01-23 18:27:44 -0600

    updated setup.py and dist folder with newest version.

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from distutils.core import setup
 setup(
   name = 'streamml2',         # How you named your package folder (MyLib)
   packages = ['streamml2'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Streamlined machine learning for transformation, feature selection, and model selection.',   # Give a short description about your library
   author = 'Blake Conrad',                   # Type in your name
   author_email = 'bmc.cs@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/conradbm/streamml2',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/conradbm/streamml2/archive/v_02.tar.gz',    # I explain this later on
-  keywords = ['Machine Learning', 'Model Selection', 'Transformation', "Feature Selection"],   # Keywords that define your package best
+  download_url = 'https://github.com/conradbm/streamml2/archive/v_03.tar.gz',    # I explain this later on
+  keywords = ['Machine Learning', 'Model Selection', 'Transformation', "Feature Selection", "Statistics",""],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
@@ -31,9 +31,7 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
